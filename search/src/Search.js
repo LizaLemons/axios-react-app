78a9899bda831beaba483bfa626426956abe7b38
Jesse Sessler <jesse.sessler@gmail.com>
2016-08-01 07:19:12 -0400

    initial

--- search/src/Search.js
+++ search/src/Search.js
@@ -3,29 +3,43 @@ import getThis from './utils/helpers.js';
 
 
 class Search extends Component {
+  // Invoked once before component mounts; returns this.state 	
   constructor(props){
+    // wake up our ancestors
     super(props);
     console.log(props);
+
+    // this is where we set our state
     this.state = {
       searchText: "",
       response: []
     };
   }
+  
+  // this will make the ajax call 
   handleClick(event) {
-    console.log(this);
+  	// THIS here is the Search class
+    console.log("THIS:", this);
+
     //this.setState({ searchText: event.target.value });
+
+    // this fxn imported from helpers.js 
+    // it makes a get req. to nyc open data & returns with a response (res)
     getThis()
     .then((res) => {
+      console.log("RES.DATA", res.data);
       this.setState({
-        response: res.data
+        response: res.data  
       });
-      console.log(res.data);
     });
   }
+
+  // this displays what you're searching for 
   handleChange(event){
     console.log(event.target.value);
     this.setState({ searchText: event.target.value });
   }
+  
   render(){
     const restaurants = this.state.response;
     const index = 0;
--- search/src/utils/helpers.js
+++ search/src/utils/helpers.js
@@ -3,3 +3,12 @@ import axios from 'axios';
 export default function(){
   return axios.get('https://data.cityofnewyork.us/resource/xx67-kt59.json');
 };
+
+
+// the above is the same as: 
+
+// const getThis = function() {
+//   return axios.get('https://data.cityofnewyork.us/resource/xx67-kt59.json');	
+// }
+
+// export default getThis;
